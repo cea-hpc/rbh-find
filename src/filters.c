2970ee9676f1f2e25b2ffdef1aa8105b62f190e5
Quentin Bouget <quentin.bouget@cea.fr>
2018-10-30 08:56:28 +0000

    Add support for the -[acm]min predicate
    Change-Id: I0fb8806ccbf2e2a6d55187fd1599d8433a44afae
    Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>

--- src/filters.c
+++ src/filters.c
@@ -16,9 +16,13 @@
 
 #include <errno.h>
 #include <error.h>
+#include <limits.h>
 #include <stdlib.h>
 
 static const enum rbh_filter_field predicate2filter_field[] = {
+    [PRED_AMIN]     = RBH_FF_ATIME,
+    [PRED_MMIN]     = RBH_FF_MTIME,
+    [PRED_CMIN]     = RBH_FF_CTIME,
     [PRED_INAME]    = RBH_FF_NAME,
     [PRED_NAME]     = RBH_FF_NAME,
 };
@@ -43,4 +47,91 @@ shell_regex2filter(enum predicate predicate, const char *shell_regex,
                       "building a regex filter for %s", pcre);
     free(pcre);
     return filter;
+}
+
+static struct rbh_filter *
+time_equal_filter_new(enum rbh_filter_field field, enum time_unit unit,
+                      time_t time)
+{
+    const struct rbh_filter *filters[2];
+    unsigned long half;
+
+    half = TIME_UNIT2SECONDS[unit] / 2;
+
+    filters[0] = rbh_filter_compare_time_new(RBH_FOP_GREATER_THAN, field,
+                                             time - half);
+    if (filters[0] == NULL)
+        error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
+                      "rbh_filter_compare_time");
+
+    filters[1] = rbh_filter_compare_time_new(RBH_FOP_LOWER_THAN, field,
+                                             time + half);
+    if (filters[1] == NULL)
+        error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
+                      "rbh_filter_compare_time");
+
+    return rbh_filter_and_new(filters, 2);
+}
+
+static struct rbh_filter *
+timedelta2filter(enum predicate predicate, enum time_unit unit,
+                 const char *_timedelta)
+{
+    enum rbh_filter_field field = predicate2filter_field[predicate];
+    const char *timedelta = _timedelta;
+    char operator = *timedelta;
+    struct rbh_filter *filter;
+    unsigned long delta; /* in seconds */
+    time_t now, then;
+    int save_errno;
+
+    switch (operator) {
+    case '-':
+    case '+':
+        timedelta++;
+    }
+
+    /* Convert the time string to a number of seconds */
+    save_errno = errno;
+    errno = 0;
+    delta = str2seconds(unit, timedelta);
+    if ((errno == ERANGE && delta == ULONG_MAX) || (errno != 0 && delta == 0))
+        error(EXIT_FAILURE, 0, "invalid argument `%s' to `%s'", _timedelta,
+              predicate2str(predicate));
+    errno = save_errno;
+
+    /* Compute `then' */
+    now = time(NULL);
+    if (now < 0)
+        error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__, "time");
+    then = now < delta ? 0 : now - delta;
+
+    switch (operator) {
+    case '-':
+        filter = rbh_filter_compare_time_new(RBH_FOP_GREATER_THAN, field, then);
+        if (filter == NULL)
+            error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
+                          "rbh_filter_compare_time_new");
+        break;
+    case '+':
+        filter = rbh_filter_compare_time_new(RBH_FOP_LOWER_THAN, field, then);
+        if (filter == NULL)
+            error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
+                          "rbh_filter_compare_time_new");
+        break;
+    default:
+        filter = time_equal_filter_new(field, unit, then);
+        if (filter == NULL)
+            error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
+                          "time_equal_filter_new");
+        break;
+    }
+
+    return filter;
+}
+
+struct rbh_filter *
+xmin2filter(enum predicate predicate, const char *minutes)
+{
+    return timedelta2filter(predicate, TU_MINUTE, minutes);
 }
