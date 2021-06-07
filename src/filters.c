b81d51ac4ec1bc3a414a662c973f596b9c0d4c90
Courrier Guillaume <guillaume.courrier@cea.fr>
2021-06-07 17:30:32 +0200

    filters: add "-perm" predicate for filtering
    The "-perm" predicate works like GNU find's option. The '+' suffix has
    been deprecated by GNU-find but can still be parsed and the option has
    no effect. Here, the '+' suffix causes the command to fail.
    
    Change-Id: Idc5a25f2e3a6b6e6deadd4761939a40dc15d939b
    Signed-off-by: Courrier Guillaume <guillaume.courrier@cea.fr>

--- src/filters.c
+++ src/filters.c
@@ -13,8 +13,10 @@
 #include <errno.h>
 #include <error.h>
 #include <limits.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sysexits.h>
+#include <ctype.h>
 
 #include <sys/stat.h>
 #ifndef HAVE_STATX
@@ -36,6 +38,7 @@ static const struct rbh_filter_field predicate2filter_field[] = {
     [PRED_MMIN]     = { .fsentry = RBH_FP_STATX, .statx = STATX_MTIME, },
     [PRED_MTIME]    = { .fsentry = RBH_FP_STATX, .statx = STATX_MTIME, },
     [PRED_TYPE]     = { .fsentry = RBH_FP_STATX, .statx = STATX_TYPE },
+    [PRED_PERM]     = { .fsentry = RBH_FP_STATX, .statx = STATX_MODE },
 };
 
 struct rbh_filter *
@@ -192,6 +195,254 @@ filetype2filter(const char *_filetype)
     filter = rbh_filter_compare_int32_new(RBH_FOP_EQUAL,
                                           &predicate2filter_field[PRED_TYPE],
                                           filetype);
+    if (filter == NULL)
+        error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
+                      "filter_compare_integer");
+
+    return filter;
+}
+
+static mode_t
+parse_symbolic_permissions(bool user, bool group, bool other, bool all,
+                           const char **input, mode_t mode)
+{
+    int who = (user || group || other);
+    mode_t perm = 0;
+
+    while (true) {
+        switch (**input) {
+        case 'r':
+            perm |= user ? 0400 : 0;
+            perm |= group ? 0040 : 0;
+            perm |= other ? 0004 : 0;
+            perm |= who ? 0 : 0444;
+            break;
+        case 'w':
+            perm |= user ? 0200 : 0;
+            perm |= group ? 0020 : 0;
+            perm |= other ? 0002 : 0;
+            perm |= who ? 0 : 0222;
+            break;
+        case 'x':
+            perm |= user ? 0100 : 0;
+            perm |= group ? 0010 : 0;
+            perm |= other ? 0001 : 0;
+            perm |= who ? 0 : 0111;
+            break;
+        case 'X':
+            /* Adds execute permission to 'u', 'g' and/or 'o' if specified and
+             * either 'u', 'g' or 'o' already has execute permissions.
+             */
+            if ((mode & 0111) != 0) {
+                perm |= user ? 0100 : 0;
+                perm |= group ? 0010 : 0;
+                perm |= other ? 0001 : 0;
+            }
+            break;
+        case 's':
+            /* 's' is ignored if only 'o' is given, it's not an error */
+            if (other && !group && !user)
+                break;
+            perm |= user  ? S_ISUID : 0;
+            perm |= group ? S_ISGID : 0;
+            break;
+        case 't':
+            /* 't' should be used when 'a' is given or who is empty */
+            perm |= (!who || all) ? S_ISVTX : 0;
+            /* using ugo with t is no an error but does nothing */
+            break;
+        default:
+            return perm;
+            break;
+        }
+        (*input)++;
+    }
+}
+
+static mode_t
+parse_symbolic_copy_perm(bool user, bool group, bool other, const char **input,
+                         mode_t mode)
+{
+    mode_t previous_flags = 0;
+    mode_t perm = 0;
+
+    switch (**input) {
+    case 'u':
+        previous_flags = (mode & 0700);
+        perm |= user ? previous_flags : 0;
+        perm |= group ? (previous_flags >> 3) : 0;
+        perm |= other ? (previous_flags >> 6) : 0;
+        (*input)++;
+        break;
+    case 'g':
+        previous_flags = (mode & 0070);
+        perm |= user ? (previous_flags << 3) : 0;
+        perm |= group ? previous_flags : 0;
+        perm |= other ? (previous_flags >> 3) : 0;
+        (*input)++;
+        break;
+    case 'o':
+        previous_flags = (mode & 0007);
+        perm |= user ? (previous_flags << 6) : 0;
+        perm |= group ? (previous_flags << 3) : 0;
+        perm |= other ? previous_flags : 0;
+        (*input)++;
+        break;
+    default:
+        break;
+    }
+
+    return perm;
+}
+
+static void
+parse_symbolic_who(const char **input, bool *user, bool *group, bool *other,
+                   bool *all)
+{
+
+    while (true) {
+        switch (**input) {
+        case 'u':
+            *user = true;
+            break;
+        case 'g':
+            *group = true;
+            break;
+        case 'o':
+            *other = true;
+            break;
+        case 'a':
+            *user = *group = *other = true;
+            *all = true;
+            break;
+        default:
+            return;
+        }
+        (*input)++;
+    }
+
+}
+
+static int
+parse_symbolic(const char *input, uint32_t *mode, const char **end)
+{
+    bool user, group, other;
+    mode_t perm;
+    bool all;
+    char op;
+
+    user = group = other = false;
+    *end = input;
+    all = false;
+    perm = 0;
+
+    parse_symbolic_who(&input, &user, &group, &other, &all);
+
+    if (*input == '-' || *input == '+' || *input == '=')
+        op = *input++;
+    else
+        /* operation is required */
+        return -1;
+
+    /* get the flags in mode if 'u', 'o' and/or 'g' are used in the perm */
+    perm |= parse_symbolic_copy_perm(user, group, other, &input, *mode);
+
+    /* if 'u', 'g' or 'o' is used here,
+     * there must be the only character to parse.
+     */
+    if (perm != 0 && (*input != '\0' && *input != ','))
+        return -1;
+
+    /* parse perm's 'r', 'w', 'x', 's', 't' and 'X' flags */
+    perm |= parse_symbolic_permissions(user, group, other, all, &input, *mode);
+
+    switch (op) {
+    case '-':
+        /* remove the flags from mode */
+        *mode &= ~perm;
+        break;
+    case '+':
+        /* add the flags to mode */
+        *mode |= perm;
+        break;
+    case '=':
+        if (perm != 0)
+            /* set the flags of mode to perm */
+            *mode = perm;
+        break;
+    }
+
+    *end = input;
+    return 0;
+}
+
+static int
+str2mode(const char *input, uint32_t *mode)
+{
+    if (*input >= '0' && *input <= '7') {
+        /* parse octal representation */
+        char *end;
+
+        errno = 0;
+        *mode = strtoul(input, &end, 8);
+
+        if (errno != 0 || *mode > 07777 || *end != '\0') {
+            *mode = 0;
+            return -1;
+        }
+    } else if (*input == '8' || *input == '9') {
+        /* error: invalid octal number */
+        return -1;
+    } else {
+        /* parse coma seperated list of symbolic representation */
+        const char *end;
+        int rc;
+
+        *mode = 0;
+        end = NULL;
+
+        do {
+            rc = parse_symbolic(input, mode, &end);
+            if (rc)
+                return -1;
+            input = end+1;
+        } while (*end == ',');
+
+        if (*end != '\0')
+            return -1;
+    }
+
+    return 0;
+}
+
+struct rbh_filter *
+mode2filter(const char *mode_arg)
+{
+    struct rbh_filter *filter;
+    uint32_t mode;
+    int operator;
+
+    if (*mode_arg == '\0')
+        error(EX_USAGE, 0,
+              "arguments to -perm should contain at least one digit or a symblic mode");
+
+    operator = RBH_FOP_EQUAL;
+    if (*mode_arg == '/') {
+        operator = RBH_FOP_BITS_ANY_SET;
+        mode_arg++;
+    }
+    else if (*mode_arg == '-') {
+        operator = RBH_FOP_BITS_ALL_SET;
+        mode_arg++;
+    }
+
+    if (str2mode(mode_arg, &mode)) {
+        error(EX_USAGE, 0, "invalid mode: %s", mode_arg);
+    }
+
+    filter = rbh_filter_compare_uint32_new(operator,
+                                           &predicate2filter_field[PRED_PERM],
+                                           mode);
     if (filter == NULL)
         error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
                       "filter_compare_integer");
