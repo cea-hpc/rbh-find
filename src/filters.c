e7240e1823d9eb9826ae7dea095a690f7f4800da
Candice Astier <candice.astier@ens.uvsq.fr>
2021-07-23 14:02:43 +0200

    str2field: add "-sort/-rsort btime" sort's field
    Change-Id: Iaa5551fd598cede47f3ee86f33a913fd08e8e35a
    Signed-off-by: Candice Astier <candice.astier@ens.uvsq.fr>

--- src/filters.c
+++ src/filters.c
@@ -724,6 +724,11 @@ str2field(const char *attribute)
             field.statx = STATX_BLOCKS;
             return field;
         }
+        if (strcmp(&attribute[1], "time") == 0) {
+            field.fsentry = RBH_FP_STATX;
+            field.statx = STATX_BTIME;
+            return field;
+        }
         break;
     case 'c':
         if (strcmp(&attribute[1], "time") == 0) {
