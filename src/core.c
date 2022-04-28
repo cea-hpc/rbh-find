5cb4ff9c4ee926e969d0f2aefe5ff00b51369845
Yoann Valeri <yoann.valeri@cea.fr>
2022-04-28 15:50:31 +0200

    find: move "action" related functions to core file
    Also add 3 callbacks corresponding to what to do before
    executing an action, how to execute an action, and what
    to do after that action.
    
    Change-Id: I011cacc77156c7a98d1447cb7431b118247737e9
    Signed-off-by: Yoann Valeri <yoann.valeri@cea.fr>

--- src/core.c
+++ src/core.c
@@ -9,6 +9,8 @@
 # include <config.h>
 #endif
 
+#include <sysexits.h>
+
 #include "rbh-find/core.h"
 
 void
@@ -17,4 +19,92 @@ ctx_finish(struct find_context *ctx)
     for (size_t i = 0; i < ctx->backend_count; i++)
         rbh_backend_destroy(ctx->backends[i]);
     free(ctx->backends);
+}
+
+int
+core_pre_action(struct find_context *ctx, const int index,
+                const enum action action)
+{
+    const char *filename;
+
+    switch (action) {
+    case ACT_FLS:
+    case ACT_FPRINT:
+    case ACT_FPRINT0:
+        if (index + 1 >= ctx->argc)
+            error(EX_USAGE, 0, "missing argument to `%s'", action2str(action));
+
+        filename = ctx->argv[index + 1];
+        ctx->action_file = fopen(filename, "w");
+        if (ctx->action_file == NULL)
+            error(EXIT_FAILURE, errno, "fopen: %s", filename);
+
+        return 1;
+    default:
+        break;
+    }
+
+    return 0;
+}
+
+int
+core_exec_action(struct find_context *ctx, enum action action,
+                 struct rbh_fsentry *fsentry)
+{
+    switch (action) {
+    case ACT_PRINT:
+        /* XXX: glibc's printf() handles printf("%s", NULL) pretty well, but
+         *      I do not think this is part of any standard.
+         */
+        printf("%s\n", fsentry_path(fsentry));
+        break;
+    case ACT_PRINT0:
+        printf("%s%c", fsentry_path(fsentry), '\0');
+        break;
+    case ACT_FLS:
+        fsentry_print_ls_dils(ctx->action_file, fsentry);
+        break;
+    case ACT_FPRINT:
+        fprintf(ctx->action_file, "%s\n", fsentry_path(fsentry));
+        break;
+    case ACT_FPRINT0:
+        fprintf(ctx->action_file, "%s%c", fsentry_path(fsentry), '\0');
+        break;
+    case ACT_LS:
+        fsentry_print_ls_dils(stdout, fsentry);
+        break;
+    case ACT_COUNT:
+        return 1;
+        break;
+    case ACT_QUIT:
+        exit(0);
+        break;
+    default:
+        error(EXIT_FAILURE, ENOSYS, "%s", action2str(action));
+        break;
+    }
+
+    return 0;
+}
+
+void
+core_post_action(struct find_context *ctx, const int index,
+                 const enum action action, const size_t count)
+{
+    const char *filename;
+
+    switch (action) {
+    case ACT_COUNT:
+        printf("%lu matching entries\n", count);
+        break;
+    case ACT_FLS:
+    case ACT_FPRINT:
+    case ACT_FPRINT0:
+        filename = ctx->argv[index];
+        if (fclose(ctx->action_file))
+            error(EXIT_FAILURE, errno, "fclose: %s", filename);
+        break;
+    default:
+        break;
+    }
 }
